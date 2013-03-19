a777f8d78475879d601986b4d9cf81e677c6f500
Gary Gross <eightyoctane@yahoo.com>
2013-03-19 13:07:17 -0700

    first commit

---
+++ target/scala-2.9.1/src_managed/main/controllers/routes.java
@@ -0,0 +1,19 @@
+// @SOURCE:/Users/ggross/Dropbox/Work/play-work/todolist/conf/routes
+// @HASH:52bc6a7c64b62015337c98cdb45b9fa139b8770a
+// @DATE:Mon Mar 18 14:59:41 PDT 2013
+
+package controllers;
+
+public class routes {
+public static final controllers.ReverseApplication Application = new controllers.ReverseApplication();
+public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets();
+public static class javascript {
+public static final controllers.javascript.ReverseApplication Application = new controllers.javascript.ReverseApplication();
+public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets();    
+}   
+public static class ref {
+public static final controllers.ref.ReverseApplication Application = new controllers.ref.ReverseApplication();
+public static final controllers.ref.ReverseAssets Assets = new controllers.ref.ReverseAssets();    
+} 
+}
+                            
